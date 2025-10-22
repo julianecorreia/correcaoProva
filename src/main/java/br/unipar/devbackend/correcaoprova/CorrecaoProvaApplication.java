42b4ce64641abb72ba885e0b255fe53dbe098545
juFrabel <15932624+juFrabel@users.noreply.github.com>
2025-10-21 21:26:04 -0300

    Initial commit

---
+++ src/main/java/br/unipar/devbackend/correcaoprova/CorrecaoProvaApplication.java
@@ -0,0 +1,13 @@
+package br.unipar.devbackend.correcaoprova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrecaoProvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorrecaoProvaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/unipar/devbackend/correcaoprova/CorrecaoProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unipar.devbackend.correcaoprova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrecaoProvaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
